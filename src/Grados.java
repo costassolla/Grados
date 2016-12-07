0f2418736344993b890df1824fd21cdbb9abd23b
Charlye <costassolla@gmail.com>
2016-12-07 08:38:31 +0100

    Segundo commit

---
+++ src/Grados.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Grados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double c;
+		double f;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduzca la temperatura en grados centígrados: ");
+
+		c = sc.nextDouble();
+
+		f = 32 + (9 * c / 5);
+
+		System.out.println(f + " grados Farenheit");
+
+		sc.close();
+	}
+
+}
